31c471ba44077ed8354454cb091eeca68c61e4ca
Joy Renee Mugo <mugojoy24@gmail.com>
2024-04-11 18:10:09 +0300

    fetch complete

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const URL = "https://anapioficeandfire.com/api/books";
+	return fetch(URL)
+		.then((response) => response.json())
+		.then((data) => renderBooks(data))
+		.catch((error) =>
+			console.error("Error fetching books:", error)
+		);
 }
 
 function renderBooks(books) {
